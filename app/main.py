8af655c1dc63092c9777aad1dd02d7ed8cd6720c
ykNe0372 <130669282+ykNe0372@users.noreply.github.com>
2025-05-06 00:20:38 +0900

    v0.2 スラッシュコマンドに変更

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('/hello'):
         await message.channel.send('Hello!')
 
 server_thread()
